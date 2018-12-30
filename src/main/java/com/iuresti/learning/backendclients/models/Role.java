3afdefb0542c1b8ad2ab92ccb4723b8a1178faf8
iuresti <iuresti@skytouchtechnology.com>
2018-12-29 23:36:26 -0600

    Add role and user entities

---
+++ src/main/java/com/iuresti/learning/backendclients/models/Role.java
@@ -0,0 +1,40 @@
+package com.iuresti.learning.backendclients.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = -8590117734896105060L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 50)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
